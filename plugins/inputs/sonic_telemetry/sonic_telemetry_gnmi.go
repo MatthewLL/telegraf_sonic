d18232731941b768f3faf0b515100d306be01cec
MatthewLL <140769379+MatthewLL@users.noreply.github.com>
2023-09-15 17:30:38 +0800

    Update sonic_telemetry_gnmi.go

--- plugins/inputs/sonic_telemetry/sonic_telemetry_gnmi.go
+++ plugins/inputs/sonic_telemetry/sonic_telemetry_gnmi.go
@@ -546,71 +546,61 @@ func (c *SONiCTelemetryGNMI) Stop() {
 }
 
 const sampleConfig = `
- ## Address and port of the GNMI GRPC server
- addresses = ["localhost:8080"]
+   ## Address and port of the GNMI GRPC server，which should be a SONiC Switch with Telemetry enabled，port is 8080 as default
+  addresses = ["172.20.222.97:8085"]
 
- ## define credentials
- username = "admin"
- password = "YourPaSsWoRd"
+  ## define credentials
+  #username = "admin"
+  #password = "YourPaSsWoRd"
 
- ## GNMI encoding requested (one of: "proto", "json", "json_ietf")
- encoding = "json_ietf"
+  ## GNMI encoding requested (one of: "proto", "json", "json_ietf")
+  encoding = "json_ietf"
 
- ## redial in case of failures after
- redial = "10s"
+  ## redial in case of failures after
+  redial = "10s"
 
- ## enable client-side TLS and define CA to authenticate the device
- enable_tls = true
- # tls_ca = "/etc/telegraf/ca.pem"
- insecure_skip_verify = true
+  ## enable client-side TLS and define CA to authenticate the device
+  #enable_tls = true
+  # tls_ca = "/etc/telegraf/ca.pem"
+  #insecure_skip_verify = true
 
- ## define client-side TLS certificate & key to authenticate to the device
- # tls_cert = "/etc/telegraf/cert.pem"
- # tls_key = "/etc/telegraf/key.pem"
+  ## define client-side TLS certificate & key to authenticate to the device
+  # tls_cert = "/etc/telegraf/cert.pem"
+  # tls_key = "/etc/telegraf/key.pem"
 
- ## GNMI subscription prefix (optional, can usually be left empty)
- ## See: https://github.com/openconfig/reference/blob/master/rpc/gnmi/gnmi-specification.md#222-paths
- # origin = ""
- # prefix = ""
- target = "OC-YANG"
-
- ## Define additional aliases to map telemetry encoding paths to simple measurement names
- #[inputs.sonic_telemetry_gnmi.aliases]
- #  ifcounters = "openconfig:/interfaces/interface/state/counters"
-
- [[inputs.sonic_telemetry_gnmi.subscription]]
-  ## Name of the measurement that will be emitted
-  name = "ifcounters"
-
-  ## Origin and path of the subscription
+  ## GNMI subscription prefix (optional, can usually be left empty)
   ## See: https://github.com/openconfig/reference/blob/master/rpc/gnmi/gnmi-specification.md#222-paths
-  ##
-  ## origin usually refers to a (YANG) data model implemented by the device
-  ## and path to a specific substructe inside it that should be subscribed to (similar to an XPath)
-  ## YANG models can be found e.g. here: https://sonic_mgmt_ip_address/ui
-  origin = ""
-  path = "/openconfig-interfaces:interfaces/interface/[name=Ethernet0]state/counters"
+  # origin = ""
+  # prefix = ""
+  #target is a database name or "OTHERS"
+  target = "COUNTERS_DB"
 
-  # Subscription mode (one of: "target_defined", "sample", "on_change") and interval
-  subscription_mode = "target_defined"
-  sample_interval = "20s"
-
-  ## Suppress redundant transmissions when measured values are unchanged
-  # suppress_redundant = false
-
-  ## If suppression is enabled, send updates at least every X seconds anyway
-  # heartbeat_interval = "60s"
+  ## Define additional aliases to map telemetry encoding paths to simple measurement names
+  # [inputs.sonic_telemetry_gnmi.aliases]
+  #   ifcounters = "openconfig:/interfaces/interface/state/counters"
 
   [[inputs.sonic_telemetry_gnmi.subscription]]
-   name = "descr"
-   origin = "openconfig-interfaces"
-   path = "/interfaces/interface/state/description"
-   subscription_mode = "on_change"
-   ## If tag_only is set, the subscription in question will be utilized to maintain a map of
-   ## tags to apply to other measurements emitted by the plugin, by matching path keys
-   ## All fields from the tag-only subscription will be applied as tags to other readings,
-   ## in the format <name>_<fieldBase>.
-   tag_only = true
+    ## Name of the measurement that will be emitted
+    name = "ifcounters"
+
+    ## Origin and path of the subscription
+    ## See: https://github.com/openconfig/reference/blob/master/rpc/gnmi/gnmi-specification.md#222-paths
+    ##
+    ## origin usually refers to a (YANG) data model implemented by the device
+    ## and path to a specific substructe inside it that should be subscribed to (similar to an XPath)
+    ## YANG models can be found e.g. here: https://sonic_mgmt_ip_address/ui
+    origin = ""
+    path = "/COUNTERS/Ethernet0"
+
+    # Subscription mode (one of: "target_defined", "sample", "on_change") and interval
+    subscription_mode = "sample"
+    sample_interval = "5s"
+
+    ## Suppress redundant transmissions when measured values are unchanged
+    # suppress_redundant = false
+
+    ## If suppression is enabled, send updates at least every X seconds anyway
+    # heartbeat_interval = "60s"
 `
 
 // SampleConfig of plugin
